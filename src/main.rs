ccdabd04e805b5cf9d0da01b2a1d7ca5d64b740b
maxjmohr <mamohoc@yahoo.com>
2024-02-20 16:45:23 +0100

    Sections WIP

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ use yew::prelude::*;
 fn app() -> Html {
     html! {
         <>
-            <h1>{ "Max Mohr" }</h1>
-            <div>
-                <h2>{ "Hey, I'm Max." }</h2>
-                <p>{ "I am currently studying data science." }</p>
+            <nav>
+                <a href="#aboutme">{"About Me"}</a>
+                <a href="#profexper">{"Professional experience"}</a>
+            </nav>
+            <div id="aboutme" class="section" style="height: 100vh; background-color: lightblue;">
+                <h2>{"About me"}</h2>
+                <p>{"Hey, I'm Max."}</p>
+                <p>{"I am currently studying data science."}</p>
+            </div>
+            <div id="profexper" class="section" style="height: 100vh; background-color: lightgreen;">
+                <h2>{"Professional experience"}</h2>
+                <p>{"Career steps go here"}</p>
             </div>
         </>
     }
