0d361af1cefbb03d93c5fa1eb3851247135781a6
maxjmohr <mamohoc@yahoo.com>
2024-02-21 16:42:06 +0100

    First implementation of sidebar

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,44 @@ use yew::prelude::*;
 fn app() -> Html {
     html! {
         <>
-            <nav>
-                <a href="#aboutme">{"About Me"}</a>
-                <a href="#profexper">{"Professional experience"}</a>
-            </nav>
-            <div id="aboutme" class="section" style="height: 100vh; background-color: lightblue;">
+        <aside class="sidebar">
+              <ul class="links">
+                <li>
+                  <span class="material-symbols-outlined">{"aboutme"}</span>
+                  <a href="#">{"About Me"}</a>
+                </li>
+                <li>
+                  <span class="material-symbols-outlined">{"profexper"}</span>
+                  <a href="#">{"Professional experience"}</a>
+                </li>
+                <li>
+                  <span class="material-symbols-outlined">{"educ"}</span>
+                  <a href="#">{"Education"}</a>
+                </li>
+                <li>
+                  <span class="material-symbols-outlined">{"projects"}</span>
+                  <a href="#">{"Projects"}</a>
+                </li>
+                <li>
+                  <span class="material-symbols-outlined">{"techskills"}</span>
+                  <a href="#">{"Technical skills"}</a>
+                </li>
+                <li>
+                  <span class="material-symbols-outlined">{"langs"}</span>
+                  <a href="#">{"Languages"}</a>
+                </li>
+              </ul>
+            </aside>
+            <div id="aboutme" class="bg-blue-500 text-white p-4 mt-4 float-right" style="width: 90%; height: 100vh">
                 <h2>{"About me"}</h2>
                 <p>{"Hey, I'm Max."}</p>
                 <p>{"I am currently studying data science."}</p>
             </div>
-            <div id="profexper" class="section" style="height: 100vh; background-color: lightgreen;">
+            <div id="profexper" class="bg-green-500 text-white p-4 mt-4 float-right" style="width: 90%; height: 100vh">
                 <h2>{"Professional experience"}</h2>
                 <p>{"Career steps go here"}</p>
             </div>
-            <div class="bg-red-500 text-white p-4 mt-4" style="width: 90%;">
+            <div class="bg-red-500 text-white p-4 mt-4 float-right" style="width: 90%; height: 100vh">
                 {"This div uses Tailwind CSS classes!"}
             </div>
         </>
