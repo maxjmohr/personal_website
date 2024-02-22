9ced3f6b9332843b86f02962bec0a48a37e9cac2
maxjmohr <mamohoc@yahoo.com>
2024-02-22 21:37:17 +0100

    Sidebar: add icons, simplify css

--- src/main.rs
+++ src/main.rs
@@ -4,46 +4,51 @@ use yew::prelude::*;
 fn app() -> Html {
     html! {
         <>
+        // Sidebar
         <aside class="sidebar">
-              <ul class="links">
-                <li>
-                  <span class="material-symbols-outlined">{"aboutme"}</span>
-                  <a href="#">{"About Me"}</a>
-                </li>
-                <li>
-                  <span class="material-symbols-outlined">{"profexper"}</span>
-                  <a href="#">{"Professional experience"}</a>
-                </li>
-                <li>
-                  <span class="material-symbols-outlined">{"educ"}</span>
-                  <a href="#">{"Education"}</a>
-                </li>
-                <li>
-                  <span class="material-symbols-outlined">{"projects"}</span>
-                  <a href="#">{"Projects"}</a>
-                </li>
-                <li>
-                  <span class="material-symbols-outlined">{"techskills"}</span>
-                  <a href="#">{"Technical skills"}</a>
-                </li>
-                <li>
-                  <span class="material-symbols-outlined">{"langs"}</span>
-                  <a href="#">{"Languages"}</a>
-                </li>
-              </ul>
-            </aside>
-            <div id="aboutme" class="bg-blue-500 text-white p-4 mt-4 float-right" style="width: 90%; height: 100vh">
+            <ul class="links">
+            <li>
+                <img loading="lazy" src="res/images/icons/aboutme.png" />
+                <a href="#">{"About me"}</a>
+            </li>
+            <li>
+                <img loading="lazy" src="res/images/icons/profexper.png" />
+                <a href="#">{"Professional experience"}</a>
+            </li>
+            <li>
+                <img loading="lazy" src="res/images/icons/educ.png" />
+                <a href="#">{"Education"}</a>
+            </li>
+            <li>
+                <img loading="lazy" src="res/images/icons/projects.png" />
+                <a href="#">{"Projects"}</a>
+            </li>
+            <li>
+                <img loading="lazy" src="res/images/icons/techskills.png" />
+                <a href="#">{"Technical skills"}</a>
+            </li>
+            <li>
+                <img loading="lazy" src="res/images/icons/langs.png" />
+                <a href="#">{"Languages"}</a>
+            </li>
+            </ul>
+        </aside>
+
+        // Main content
+        <div id="aboutme" class="bg-blue-500 text-white p-4 mt-4 float-right" style="width: 100%; height: 100vh">
+            <div style="padding-left: 10%;">
                 <h2>{"About me"}</h2>
                 <p>{"Hey, I'm Max."}</p>
                 <p>{"I am currently studying data science."}</p>
             </div>
-            <div id="profexper" class="bg-green-500 text-white p-4 mt-4 float-right" style="width: 90%; height: 100vh">
-                <h2>{"Professional experience"}</h2>
-                <p>{"Career steps go here"}</p>
-            </div>
-            <div class="bg-red-500 text-white p-4 mt-4 float-right" style="width: 90%; height: 100vh">
-                {"This div uses Tailwind CSS classes!"}
-            </div>
+        </div>
+        <div id="profexper" class="bg-green-500 text-white p-4 mt-4 float-right" style="width: 90%; height: 100vh">
+            <h2>{"Professional experience"}</h2>
+            <p>{"Career steps go here"}</p>
+        </div>
+        <div class="bg-red-500 text-white p-4 mt-4 float-right" style="width: 90%; height: 100vh">
+            {"This div uses Tailwind CSS classes!"}
+        </div>
         </>
     }
 }
