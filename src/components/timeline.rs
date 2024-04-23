aa5fe8db3b2fa156f7066b8613542e133089960c
maxjmohr <mamohoc@yahoo.com>
2024-04-23 10:08:48 +0200

    Try to resolve timeline dots color fill issue

--- src/components/timeline.rs
+++ src/components/timeline.rs
@@ -32,7 +32,7 @@ pub fn Timeline(props: &TimelineProps) -> Html {
     html! {
         <div class="relative pl-16 lg:pl-52 py-6 group" data-aos="fade-up">
             <a class="timeline_title">{&props.title}</a>
-                <div class={format!("flex flex-col lg:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 lg:before:left-0 before:h-full before:px-px before:bg-slate-300 dark:before:bg-neutral-500 before:ml-3.5 lg:before:ml-[11.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 lg:after:left-0 after:w-6 after:h-6 lg:after:w-4 lg:after:h-4 after:{} after:border-8 lg:after:border-4 after:box-content after:border-slate-50 dark:after:border-neutral-300 after:rounded-full after:ml-3.5 lg:after:ml-[11.5rem] after:-translate-x-1/2 after:translate-y-0.5", props.color.clone())}>
+                <div class={format!("flex flex-col lg:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 lg:before:left-0 before:h-full before:px-px before:bg-slate-300 dark:before:bg-neutral-500 before:ml-3.5 lg:before:ml-[11.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 lg:after:left-0 after:w-6 after:h-6 lg:after:w-4 lg:after:h-4 after:{} after:border-8 lg:after:border-4 after:box-content after:border-slate-50 dark:after:border-neutral-300 after:rounded-full after:ml-3.5 lg:after:ml-[11.5rem] after:-translate-x-1/2 after:translate-y-0.5", props.color)}>
                 <time class="timeline_time">{&props.time}</time>
                 <a href={props.place_url.clone()} target="_blank" class="timeline_desc italic pt-3 lg:pt-0">{&props.place}</a>
             </div>
