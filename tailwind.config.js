899ebc84b3518e5f2ed7f1e57800f77fae3387d3
maxjmohr <mamohoc@yahoo.com>
2025-01-23 21:56:01 +0100

    Preparation for upgrade to tailwind 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,26 +20,4 @@ module.exports = {
       }
     },
   },
-  plugins: [
-    require('tailwind-typewriter')({
-        wordsets: {
-            aboutme: {
-              words: [fs.readFileSync('res/content/aboutme.md', 'utf8')],
-              writeSpeed: 0.05,
-              pauseBetween: 1,
-              repeat: 0,
-              eraseSpeed: 0,
-              caretWidth: '3px'
-            },
-            langs: {
-                words: ['ét un peu...', "e un po'...", 'y un poco...'],
-                writeSpeed: 0.05,
-                pauseBetween: 1,
-                repeat: -1,
-                eraseSpeed: 0.05,
-                caretWidth: '3px'
-              }
-        }
-    })
-  ],
 }
